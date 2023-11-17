3d267699c6bdfbc173e6fdb77114f686599bafce
min4604 <46911074+min4604@users.noreply.github.com>
2023-11-17 15:58:59 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,52 +1,6 @@
-#include <iostream>
-#include <stdlib.h>
-#include <list>
-#include <cmath>
-#include <time.h>
-
-#define REPEAT 10
-
-using namespace std;
-
-int main() {
-    srand(time(NULL));
-    cout << "重複 " << REPEAT << " 次取平均" << "\n";
-    for (int k = 1; k <31 ; k++)
-    {
-        
-        double addTotalSpendTime = 0;
-        double sumTotalSpendTime = 0;
-        for (int n = 0; n < REPEAT; n++)
-        {
-            list<int> linkedList;
-            double START, END;
-            START = clock();
-            for (int i = 0; i < pow(2, k); i++)
-            {
-                linkedList.push_back(rand());
-            }
-            END = clock();
-
-            addTotalSpendTime += ((END - START) / CLOCKS_PER_SEC);
-
-
-            double sum_START, sum_END;
-            sum_START = clock();
-            int sum = 0;
-            for (int n : linkedList)
-            {
-                sum += n;
-            }
-            sum_END = clock();
-            sumTotalSpendTime += ((sum_END - sum_START) / CLOCKS_PER_SEC);
-        }
-        double addAvrgSpendTime = addTotalSpendTime / REPEAT;
-        double sumAvrgSpendTime = sumTotalSpendTime / REPEAT;
-        cout << endl << "Link List新增2^" << k << "個隨機數所需的時間:" << addAvrgSpendTime << " sec" << endl;
-        cout << endl << "Link List新增2^" << k << "個隨機數後計算總合所需的時間:" << sumAvrgSpendTime << " sec" << endl << endl;
-    }
-
-    return 0;
-}
-
-
+import pandas as pd
+import random
+data=pd.read_csv('data.csv')
+data["Last Name"] = data["Full Name"].str.split().str[-1]
+students_lin = data[data["Last Name"]=="Lin"]
+print(students_lin['Full Name'].values[random.randint(0,len(students_lin)-1)])
